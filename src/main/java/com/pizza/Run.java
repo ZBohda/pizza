bb2704ec652683f9c167f86efdd3e4ac2081e9e5
cyber <zamkowy.bogdan@gmail.com>
2018-07-27 14:38:47 +0300

    Fixed a web.xml

--- src/main/java/com/pizza/Run.java
+++ src/main/java/com/pizza/Run.java
@@ -1,7 +1,10 @@
 package com.pizza;
 
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Run {
     public static void main(String... args) {
-
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
     }
 }
